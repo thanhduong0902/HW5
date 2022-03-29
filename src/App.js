004fb6086b1584fbc63b0ec4006c63734907a4b8
unknown <thanhpot88@gmail.com>
2022-03-29 17:21:57 +0700

    o

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [light1, setLight1] = useState(true);
+  const [light2, setLight2] = useState(false);
+  const [light3, setLight3] = useState(false);
+  const change = () => {
+    if (light1 === true) {
+      setLight1("light1-off");
+      setLight2(true);
+    } else {
+      if (light2 === true) {
+        setLight2("light2-off");
+        setLight3(true);
+      } else {
+        if (light3 === true) {
+          setLight3("light3-off");
+          setLight1(true);
+        }
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="next" onClick={change}>
+        Next
+      </button>
+      <div
+        className={light1 === true ? "light1-on led" : "light1-off led"}
+      ></div>
+      <div
+        className={light2 === true ? "light2-on led" : "light2-off led"}
+      ></div>
+      <div
+        className={light3 === true ? "light3-on led" : "light3-off led"}
+      ></div>
     </div>
   );
 }
